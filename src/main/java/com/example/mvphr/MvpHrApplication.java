748c5768fbc430fcf140229c1a2daaceba4a46d3
Sky SeVen <shrikelionel0@gmail.com>
2024-04-13 03:22:45 +0300

    Initial Commit.

---
+++ src/main/java/com/example/mvphr/MvpHrApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvphr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpHrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvpHrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mvphr/MvpHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvphr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpHrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
